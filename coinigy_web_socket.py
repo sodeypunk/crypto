002b78c60a68f20277fd594c85a7332dd6a08301
soda heng <soda.heng@gmail.com>
2017-12-21 00:08:34 -0600

    Multiple exchange and coins

--- coinigy_web_socket.py
+++ coinigy_web_socket.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from dateutil import tz
 from socketclusterclient import Socketcluster
 logging.basicConfig(format="%s(levelname)s:%(message)s", level=logging.DEBUG)
+pd.options.display.precision = 10
 
 api_credentials = json.loads('{}')
 api_credentials["apiKey"]=credentials.Coinigy.API
@@ -15,12 +16,17 @@ api_credentials["apiSecret"]=credentials.Coinigy.Secret
 def your_code_starts_here(socket):
 
     ###Code for subscription
-    #socket.subscribe('ORDER-BTRX--BCC--BTC')                 # Channel to be subscribed
-    #socket.subscribe('ORDER-BTRX--XRP--BTC')                 # Channel to be subscribed
-    #socket.subscribe('ORDER-BTRX--XLM--BTC')                 # Channel to be subscribed
+    socket.subscribe('ORDER-BTRX--BCC--BTC')                 # Channel to be subscribed
+    socket.subscribe('ORDER-BTRX--XRP--BTC')                 # Channel to be subscribed
+    socket.subscribe('ORDER-BTRX--XLM--BTC')                 # Channel to be subscribed
     socket.subscribe('ORDER-BTRX--LTC--BTC')                 # Channel to be subscribed
 
-    market = namedtuple('market', ('exchange', 'symbol', 'average_price', 'average_price_min_vol', 'volume', 'is_min_vol', 'rows', 'age'))
+    socket.subscribe('ORDER-BINA--BCC--BTC')                 # Channel to be subscribed
+    socket.subscribe('ORDER-BINA--XRP--BTC')                 # Channel to be subscribed
+    socket.subscribe('ORDER-BINA--XLM--BTC')                 # Channel to be subscribed
+    socket.subscribe('ORDER-BINA--LTC--BTC')                 # Channel to be subscribed
+
+    market = namedtuple('market', ('exchange', 'symbol', 'average_price', 'average_price_min_vol', 'volume', 'is_min_vol', 'rows'))
     market_dict = {}
     from_zone = tz.tzutc()
     to_zone = tz.tzlocal()
@@ -32,36 +38,52 @@ def your_code_starts_here(socket):
 
         return diff
 
+    def calcArb(symbol, min_arb):
+        # TODO convert market_dict into a pandas frame for more efficient search and calculation
+
+        for key in market_dict:
+            if (symbol in market_dict[key] and market_dict[key][symbol].is_min_vol):
+                for key2 in market_dict:
+                    if (key2 != key and symbol in market_dict[key2] and market_dict[key2][symbol].is_min_vol):
+                        price1 = market_dict[key][symbol].average_price_min_vol
+                        price2 = market_dict[key2][symbol].average_price_min_vol
+                        arb = float(price2 - price1) / price1
+
+                        print(symbol + ": price1: " + str(price1) + ", price2: " + str(price2) + ", arb1: " + str(arb))
+                        if (arb > min_arb):
+                            print("\n\n*** " + symbol + " Arb opportunity from " + key + " to " + key2 + " for " + str(arb*100) + "% ***")
+
 
     def channelmessage(key, data):                         # Messages will be received here
         #print ("\n\n\nGot data "+json.dumps(data, sort_keys=True)+" from channel "+key)
         print("\n\n\n")
+        exchange = key[6:10]
+        symbol = key[12:].replace('--','/')
+
         datapd = pd.DataFrame(data)
+        datapd['price'] = pd.to_numeric(datapd['price'])
         datapd['cumu_total'] = datapd['total'].cumsum()
 
         min_vol_btc = 0.5
+        min_arb =.01
         rows = len(datapd)
 
-        utc_timestamp = datapd['timestamp'][0]
-        exchange = datapd['exchange'][0]
-        symbol = datapd['label'][0]
+        #utc_timestamp = datapd['timestamp'][0]
+
         volume = datapd['total'].sum()
         index_list_min_vol = datapd.where(datapd['cumu_total'] >= min_vol_btc).dropna().index
         is_min_vol = len(index_list_min_vol) > 0
         average_price = datapd['price'].mean()
 
         if (is_min_vol):
-            row_index = index_list_min_vol[0]
+            row_index = index_list_min_vol[0] + 1
             average_price_min_vol = datapd['price'][0:row_index].mean()
         else:
             average_price_min_vol = 0
 
-        age = getAge(utc_timestamp)
-
         #print(exchange + ", " + symbol + ", " + str(average_price) + ", " + str(average_price_min_vol) + ", " + str(volume) + ", " + str(is_min_vol))
         new_market = market(exchange=exchange, symbol=symbol, average_price=average_price, rows = rows,
-                            average_price_min_vol=average_price_min_vol, volume=volume, is_min_vol=is_min_vol,
-                            age=age)
+                            average_price_min_vol=average_price_min_vol, volume=volume, is_min_vol=is_min_vol)
 
         if (exchange not in market_dict):
             market_dict[exchange] = {}
@@ -69,22 +91,28 @@ def your_code_starts_here(socket):
         market_dict[exchange][symbol] = new_market
 
         print(market_dict)
+        calcArb(symbol, min_arb)
 
 
 
 
-    #socket.onchannel('ORDER-BTRX--BCC--BTC', channelmessage) # This is used for watching messages over channel
-    #socket.onchannel('ORDER-BTRX--XRP--BTC', channelmessage) # This is used for watching messages over channel
-    #socket.onchannel('ORDER-BTRX--XLM--BTC', channelmessage) # This is used for watching messages over channel
+    socket.onchannel('ORDER-BTRX--BCC--BTC', channelmessage) # This is used for watching messages over channel
+    socket.onchannel('ORDER-BTRX--XRP--BTC', channelmessage) # This is used for watching messages over channel
+    socket.onchannel('ORDER-BTRX--XLM--BTC', channelmessage) # This is used for watching messages over channel
     socket.onchannel('ORDER-BTRX--LTC--BTC', channelmessage) # This is used for watching messages over channel
+
+    socket.onchannel('ORDER-BINA--BCC--BTC', channelmessage) # This is used for watching messages over channel
+    socket.onchannel('ORDER-BINA--XRP--BTC', channelmessage) # This is used for watching messages over channel
+    socket.onchannel('ORDER-BINA--XLM--BTC', channelmessage) # This is used for watching messages over channel
+    socket.onchannel('ORDER-BINA--LTC--BTC', channelmessage) # This is used for watching messages over channel
     
     ###Code for emit
 
     def ack(eventname, error, data):
         print ("\n\n\nGot ack data " + json.dumps(data, sort_keys=True) + " and eventname is " + eventname)
     
-    #socket.emitack("exchanges",None, ack)  
-    #socket.emitack("channels", "OK", ack) 
+    socket.emitack("exchanges",None, ack)
+    socket.emitack("channels", "OK", ack)
 
 def onconnect(socket):
     logging.info("on connect got called")
